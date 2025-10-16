563f8af2e8c8d1b3679a766ac763978ed38977cf
Elie Uzan <elie.uzan@yahoo.fr>
2025-10-16 18:52:29 +0200

    on a créé un accordeon dans welcome.blade + install de @alpinejs/collapse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,27 @@
 <x-layout>
     <p>Bienvenue sur la page d'accueil !</p>
+
+    <div 
+        class="mt-4 w-fullbg-white rounded-xl shadow"
+        x-data="{
+            open: false,
+            }"
+    >
+        <button 
+            type="button" 
+            class="btn btn-primary w-full" 
+           x-on:click="open = !open"
+        >
+        Titre de l'accordéon</button>
+        <div
+            x-show="open"
+            x-collapse
+        >
+            <div 
+                class="p-3" 
+            >
+                Contenu de l'accordéon
+            </div>
+        </div>
+    </div>
 </x-layout>
